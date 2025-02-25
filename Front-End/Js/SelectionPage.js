79fed063ebfc16f73b7c2ba4b0242543edf04f40
Jazxngh <singhjaspinder528@gmail.com>
2025-02-25 11:39:08 +0000

    BeforeLoginDB

--- Front-End/Js/SelectionPage.js
+++ Front-End/Js/SelectionPage.js
@@ -5,10 +5,10 @@ const adminSupportBtn = document.getElementById('S-AdminSupportBtn');
 // Both Event listener 
 userBtn.addEventListener('click', function() {
   // Takes you to the standard page
-  window.location.href = 'Front-End/LoginPage.html';
+  window.location.href = 'LoginPage.html';
 });
 
 adminSupportBtn.addEventListener('click', function() {
   // Takes you to admin/support page
-  window.location.href = 'Front-End/ASLoginPage.html';
+  window.location.href = 'ASLoginPage.html';
 });
